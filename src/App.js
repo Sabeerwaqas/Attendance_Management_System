d6d60facffeee054ed4c6561a5072a5536a9c0a0
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-07-18 23:05:06 +0500

    [Feat: Make other component and link togeher]

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import Signup from "./Components/SignUp/Signup";
 import Login from "./Components/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ProtectedRoute from "./Components/Context/ProtectedRoute";
-import Main from "./Components/Main'/Main"
-
+import Main from "./Components/Main/Main";
+import Sidebar from "./Components/Main/Sidebar/Sidebar";
+import Classes from "./Components/Classes/Classes";
+import Teacher from "./Components/Teachers/Teacher";
+import Student from "./Components/Students/Student";
 const App = () => {
   return (
     <>
-      {/* <BrowserRouter>
-      <Routes>
-        <Route path='/login' element = {<Login />}/>
-        <Route path='/signup' element = {<Signup/>}/>
-        <Route path="/" element = {<ProtectedRoute Component = {Main} />}/>
-      </Routes>
-    </BrowserRouter> */}
-
-    <Main/>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Sidebar} />
+          <Route path="/students" Component={Student} />
+          <Route path="/teachers" Component={Teacher} />
+          <Route path="/classes" Component={Classes} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import Signup from './Components/SignUp/Signup';
+// import Signup from './Components/SignUp/Signup';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
